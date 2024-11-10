d05bb51ca4df62db7df4640c2998a0b3c602c3df
Zhu Yitong <2787184571@qq..com>
2024-11-11 00:47:43 +0800

    11.11

--- exercises/traits/traits1.rs
+++ exercises/traits/traits1.rs
@@ -7,7 +7,6 @@
 // Execute `rustlings hint traits1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 trait AppendBar {
     fn append_bar(self) -> Self;
@@ -15,6 +14,9 @@ trait AppendBar {
 
 impl AppendBar for String {
     // TODO: Implement `AppendBar` for type `String`.
+    fn append_bar(self) -> String {
+        self + "Bar" // + 两侧一定是字符串切片类型
+    }
 }
 
 fn main() {
